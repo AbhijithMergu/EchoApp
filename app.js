4258b39a68f8febb56bf5b43d059b314dcaa3264
abhijith.mergu <abhijith.mergu@servicenow.com>
2020-08-11 22:26:48 +0530

    Added multi user support

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var escape = require('escape-html');
 var app = express();
 app.use(express.json());
 
-var requests = [];
+var requests = {};
 const VERIFY_TOKEN = 'nodeapp';
 const PORT = process.env.PORT || 5000
 
@@ -21,39 +21,55 @@ app.post('/sharepoint_webhook', function(req, res){
   res.status(status).end(http.STATUS_CODES[status]);
 });
 
-app.get('/getreq', function (req, res) {
-    let request = requests.shift();
+app.get('/:user/getreq', function (req, res) {
+    let user = req.params.user;
+    let request = null;
+    if(requests.hasOwnProperty(user))
+      request = requests[user].shift();
     if(request)
       res.json(request)
     else
       res.status(404).send();
 });
 
-app.post('/webhook', function (req, res) {
+app.post('/:user/webhook', function (req, res) {
+    let user = req.params.user;
     let request = {};
     request.headers = req.headers;
     request.body = req.body;
-    requests.push(request);
-    res.status(200).send();
+    if(requests.hasOwnProperty(user)){
+      requests[user].push(request);
+      res.status(200).send();
+    } else {
+      res.status(401).send();
+    }
 });
 
-app.get("/webhook", (req, res) => {
+app.get("/:user/webhook", (req, res) => {
+    let user = req.params.user;
     let mode = req.query["hub.mode"];
     let token = req.query["hub.verify_token"];
     let challenge = req.query["hub.challenge"];
-  
+
+    let reqSuccess = true;
     if (mode && token) {
       if (mode === "subscribe" && token === VERIFY_TOKEN) {
         // Responds with the challenge token from the request
         console.log("WEBHOOK_VERIFIED");
         res.status(200).send(challenge);
+        requests[user] = [];
       } else {
         // Responds with '403 Forbidden' if verify tokens do not match
-        res.sendStatus(403);
+        reqSuccess = false;
       }
+    } else{
+      reqSuccess = false;
     }
-  });
+
+    if(!reqSuccess)
+      res.sendStatus(403);
+});
 
 app.listen(PORT, function () {
-  console.log('Echo app listening on port 3000!');
+  console.log(`Echo app listening on port ${PORT}!`);
 });
