a49f151acfc830f51af73ce472bf47a79c7aee2e
abhijith.mergu <abhijith.mergu@servicenow.com>
2020-08-20 17:58:42 +0530

    Changed param to query

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.post('/:user/webhook', function (req, res) {
     let request = {};
     request.headers = req.headers;
     request.body = req.body;
-    request.params = req.params;
+    request.params = req.query;
     if(requests.hasOwnProperty(user)){
       requests[user].push(request);
       res.status(200).send();
